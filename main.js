b030422e2525d2c8728a8a8199d6435a8e0d1989
Tizian Schönijahn <104362859+TizianSchoenijahn@users.noreply.github.com>
2022-06-06 20:33:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ let integer = 0 ;
 
 var audioElement = new Audio('tsclick.wav');
 
-var successBool = window.navigator.vibrate(pattern);
-
 add.addEventListener('click',function(){
     integer += 1;
     val.innerHTML = integer;
