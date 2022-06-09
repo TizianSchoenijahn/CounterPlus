6571b118e5b4fba927d4a58f88928a4e3aef62df
Tizian Schönijahn <104362859+TizianSchoenijahn@users.noreply.github.com>
2022-06-09 18:56:56 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,57 @@
+console.log("main script loaded");
+
+console.log(localStorage)
+
+let add = document.getElementById('Plus');
+let remove = document.getElementById('Minus');
+
+let val = document.getElementById('number');
+let ref = document.getElementById('RefreshButton');
+let integer = 0 ;
+
+ref.addEventListener('click',function(){
+    integer = 0;
+    val.innerHTML = integer;
+    localStorage.setItem('number', 0)
+    window.navigator.vibrate([50, 50, 30]);
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    integer = JSON.parse(window.localStorage.getItem('number'));
+    val.innerHTML = integer;
+    window.navigator.vibrate([30, 50, 30]);
+})
+
+add.addEventListener('click',function(){
+    integer += 1;
+    val.innerHTML = integer;
+    localStorage.setItem('number', integer)
+    window.navigator.vibrate(50);
+})
+
+remove.addEventListener('click',function(){
+    integer -= 1;
+    val.innerHTML = integer;
+    localStorage.setItem('number', integer)
+    window.navigator.vibrate(50);
+})
+
+//audio
+
+var click = new Audio('sound/tsclick.wav');
+var reverseclick = new Audio('sound/tsreverseclick.wav');
+
+ref.addEventListener('click',function(){
+    reverseclick.pause();
+    reverseclick.play();
+})
+
+add.addEventListener('click',function(){
+    click.pause();
+    click.play();
+})
+
+remove.addEventListener('click',function(){
+    click.pause();
+    click.play();
+})
---
+++ script/style.js
@@ -0,0 +1,61 @@
+console.log("style script loaded");
+
+let body = document.getElementById('body');
+let header = document.getElementById('header');
+let HeaderTitle = document.getElementById('HeaderTitle');
+let number = document.getElementById('number');
+let MainButton1 = document.getElementById('Minus');
+let MainButton2 = document.getElementById('Plus');
+let infobutton = document.getElementById('infobutton');
+let RefreshIconImg = document.getElementById('RefreshIconImg');
+
+
+
+let dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (dark) {
+    body.style.backgroundColor = "rgb(30, 30, 30)";
+
+    header.style.backgroundColor = "rgb(40, 40, 40)";
+
+    RefreshIconImg.src="image/flaticon-reload-white.png";
+
+    HeaderTitle.style.color = "white";
+
+    number.style.color = "white";
+
+    MainButton1.style.color = "white";
+    MainButton1.style.backgroundColor = "rgb(60, 60, 60)";
+
+    MainButton2.style.color = "white";
+    MainButton2.style.backgroundColor = "rgb(60, 60, 60)";
+
+    infobutton.style.color = "white";
+    infobutton.style.backgroundColor = "rgb(60, 60, 60)";
+
+} else {
+
+    body.style.backgroundColor = "white";
+
+    header.style.backgroundColor = "rgb(195, 195, 195)";
+    header.style.boxShadow = " 0px 10px 50px -10px rgba(100, 100, 100, 0.5)";
+
+    RefreshIconImg.src="image/flaticon-reload-black.png";
+
+    HeaderTitle.style.color = "black";
+
+    number.style.color = "black";
+
+    MainButton1.style.color = "black";
+    MainButton1.style.backgroundColor = "rgb(215, 215, 215)";
+    MainButton1.style.boxShadow = "0px 10px 50px -10px rgba(100, 100, 100, 0.5)";
+
+    MainButton2.style.color = "black";
+    MainButton2.style.backgroundColor = "rgb(215, 215, 215)";
+    MainButton2.style.boxShadow = "0px 10px 50px -10px rgba(100, 100, 100, 0.5)";
+
+    infobutton.style.color = "black";
+    infobutton.style.backgroundColor = "rgb(215, 215, 215)";
+    infobutton.style.boxShadow = "0px 10px 50px -10px rgba(100, 100, 100, 0.5)";
+
+}
